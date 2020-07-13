cd199120fdf1cb04b83e5f48fb6aa4ae28de533f
raunakmali <raunakmali@gmail.com>
2020-07-13 09:54:21 +0530

    correcting structure

---
+++ MyMagicConfig/MyMagicConfig/Default.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MyMagicConfig
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            myDiv.InnerHtml = Common.toHTML_Table(GetDatainDataTableFormat());
+        }
+        static DataTable GetDatainDataTableFormat()
+        {
+            // Step 2: here we create a DataTable.
+            // ... We add 4 columns, each with a Type.
+            DataTable table = new DataTable();
+            table.Columns.Add("Dosage", typeof(int));
+            table.Columns.Add("Drug", typeof(string));
+            table.Columns.Add("Patient", typeof(string));
+            table.Columns.Add("Date", typeof(DateTime));
+
+            // Step 3: here we add 5 rows.
+            table.Rows.Add(25, "Indocin", "David", DateTime.Now);
+            table.Rows.Add(50, "Enebrel", "Sam", DateTime.Now);
+            table.Rows.Add(10, "Hydralazine", "Christoff", DateTime.Now);
+            table.Rows.Add(21, "Combivent", "Janet", DateTime.Now);
+            table.Rows.Add(100, "Dilantin", "Melanie", DateTime.Now);
+            return table;
+        }
+        
+    }
+}
